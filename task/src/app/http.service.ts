94f98571635e3b3aa75b1028b725b4e55a478270
rlyost <ry@ryost.us>
2018-04-12 15:13:02 -0700

    added new get methods

--- task/src/app/http.service.ts
+++ task/src/app/http.service.ts
@@ -6,6 +6,8 @@ export class HttpService {
 
   constructor(private _http: HttpClient) {
     this.getTasks();
+    this.getTaskById( '5ace78766294b517cb811934' );
+    this.destroyTask( '5ace7db6778f281896a5cfc2' );
    }
 
    getTasks(){
@@ -13,5 +15,17 @@ export class HttpService {
 
      tempObservable.subscribe(data => console.log("Got our tasks!", data));
    }
+   getTaskById(id){
+    var route_call = "/task/" + id; 
+    let tempObservable = this._http.get(route_call);
+
+    tempObservable.subscribe(data => console.log("Got our task!", data));
+  }
+  destroyTask(id){
+    var route_call = "/task/remove/" + id;
+    let tempObservable = this._http.delete(route_call);
+
+    tempObservable.subscribe(data => console.log("Task destroyed!", data));
+  }
 
 }
