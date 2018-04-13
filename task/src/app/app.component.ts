621968771a54aa52590a6ea12dae20fa882be8e2
rlyost <ry@ryost.us>
2018-04-13 13:57:21 -0700

    Added task to the template

--- task/src/app/app.component.ts
+++ task/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,10 +6,22 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MEAN';
+export class AppComponent implements OnInit {
+  title = 'Restful Task API with Angular!';
+  tasks = [];
 
   constructor(private _httpService: HttpService){
-    
+
+  }
+  ngOnInit(){
+    this.getTasksFromService();
+  }
+  getTasksFromService(){
+    let observable = this._httpService.getTasks()
+    observable.subscribe(data => {
+      console.log("Got our data!", data);
+      this.tasks = data['data'];
+      console.log(this.tasks)
+    });
   }
 }
--- task/src/app/http.service.ts
+++ task/src/app/http.service.ts
@@ -5,21 +5,14 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
 
   constructor(private _http: HttpClient) {
-    this.getTasks();
-    this.getTaskById( '5ace78766294b517cb811934' );
-    this.destroyTask( '5ace7db6778f281896a5cfc2' );
-   }
+  }
 
    getTasks(){
-     let tempObservable = this._http.get('/task');
-
-     tempObservable.subscribe(data => console.log("Got our tasks!", data));
+     return this._http.get('/task');
    }
    getTaskById(id){
     var route_call = "/task/" + id; 
-    let tempObservable = this._http.get(route_call);
-
-    tempObservable.subscribe(data => console.log("Got our task!", data));
+    return this._http.get(route_call);
   }
   destroyTask(id){
     var route_call = "/task/remove/" + id;
